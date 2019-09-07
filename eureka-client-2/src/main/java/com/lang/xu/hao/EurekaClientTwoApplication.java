f74421efa5a94700c95ce5c7d4d443e7e857cb0d
langxuhao <511831389@qq.com>
2019-09-07 10:18:20 +0800

    eureka client-1 client-2

--- eureka-client-2/src/main/java/com/lang/xu/hao/EurekaClientTwoApplication.java
+++ eureka-client-2/src/main/java/com/lang/xu/hao/EurekaClientTwoApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaClient_2Application {
+public class EurekaClientTwoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClient_2Application.class, args);
+		SpringApplication.run(EurekaClientTwoApplication.class, args);
 	}
 }
