cdf8eec423a4199f15ee145310513f5661500a5f
langxuhao <8201234a>
2020-01-07 19:36:41 +0800

    添加：项目说明

---
+++ eureka-user/src/main/java/com/lang/xu/hao/eurekauser/eurekaserver/controller/HiController.java
@@ -0,0 +1,20 @@
+package com.lang.xu.hao.eurekauser.eurekaserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Xuhao
+ * @Description:
+ * @Date: Created in 13:40 2018/12/1
+ */
+@RestController
+public class HiController {
+
+	@RequestMapping("/hi")
+	public String hi() {
+
+		return "hi";
+	}
+
+}
---
+++ eureka-user2/README.md
@@ -0,0 +1,6 @@
+
+spring cloud erueka 客户端
+1.redis session 共享测试类:TestController
+2.分布式锁 redis+lua  RedisLock
+3.分布式锁 redisson RedissonConfigTest
+4.jedis配置
