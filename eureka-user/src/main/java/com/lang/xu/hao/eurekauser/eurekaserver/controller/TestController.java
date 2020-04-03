db19a56784b2e918ac7ea3cf946595020d6113d0
langxuhao <8201234a>
2020-04-03 17:47:09 +0800

    修改redis为集群模式 修改配置项

--- eureka-user/src/main/java/com/lang/xu/hao/eurekauser/eurekaserver/controller/TestController.java
+++ eureka-user/src/main/java/com/lang/xu/hao/eurekauser/eurekaserver/controller/TestController.java
@@ -5,6 +5,7 @@ import com.netflix.discovery.EurekaClient;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 	@Qualifier("eurekaClient")
-	@Resource
+	@Autowired
 	private EurekaClient discoveryClient;
 
 	@Resource
